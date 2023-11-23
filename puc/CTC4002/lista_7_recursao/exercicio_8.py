157b8ceef3c4b1c5e1bce4a50b1368f2accc64e5
OmarMesqq <omarmsqt@gmail.com>
2023-11-22 23:18:48 -0300

    ex 8

---
+++ puc/CTC4002/lista_7_recursao/exercicio_8.py
@@ -0,0 +1,30 @@
+"""
+Escreva uma função recursiva que receba um número inteiro (n) e um algarismo (a) 
+e "elimine" o algarismo a do número n.
+Exemplos 
+•  Número 12342 e algarismo 2 --> a função retornará 134  
+•  Número 12342 e algarismo 5 --> a função retornará 12342  
+•  Número 2 e algarismo 2 --> a função retornará 0   
+"""
+
+def eliminacao(n, a):
+    n_str = str(n)
+    
+    if len(n_str) == 1:
+        return 0 if n_str == f'{a}' else n_str
+        
+
+    if n_str[0] == f'{a}':
+        return eliminacao(n_str[1:], a)
+        
+    else: 
+        return n_str[0] + f'{eliminacao(n_str[1:], a)}'
+        
+        
+   
+        
+    
+
+print(eliminacao(12342, 2)) # deve imprimir 134
+print(eliminacao(12342, 5)) # deve imprimir 12342
+print(eliminacao(2, 2)) # deve imprimir 0 
