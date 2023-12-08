5e39c017bcb652596b13ccaf2ab2a5a492e94766
OmarMesqq <omarmsqt@gmail.com>
2023-12-08 20:23:34 -0300

    feat: termina exercicio 1

---
+++ puc/CTC4002/G2/exercicio_1.py
@@ -0,0 +1,51 @@
+"""
+São entradas de uma função:
+- cor 
+- diametro do aro 
+- e uma lista de listas da seguinte forma:
+[ ['marca_1', 'cor_1', x], ['marca_2', 'cor_2', y], ...]
+
+onde x e y são tamanhos de aros de bicicletas. 
+
+Escreva uma função que retorne também uma lista de listas no seguinte formato:
+
+[ ['marca_1', a], ['marca_2', b], ...]
+
+onde a e b são a quantidade de bicicletas (sublistas da entrada)
+que são da cor desejada e de tamanho do aro maior ou igual ao desejado 
+
+Obs.: A lista de entrada só pode ser percorrida uma vez
+"""
+
+def pegaBicicletas(cor: str, diametro: int, lista: []):
+    marcas = {}
+    achado = False
+
+    for sublista in lista: 
+        if sublista[1] == cor and sublista[2] >= diametro: 
+            for marca in marcas: 
+                if marca == sublista[0]:
+                    achado = True 
+                    marcas[marca] += 1
+                    break 
+            if achado == False:
+                marcas[sublista[0]] = 1
+
+    return marcas 
+
+
+
+corMock = 'azul'
+aroMock = 25
+listaMock = [
+    ['MarcaA', 'vermelho', 20],
+    ['MarcaB', 'azul', 24],
+    ['MarcaC', 'azul', 26],
+    ['MarcaD', 'preto', 25],
+    ['MarcaE', 'azul', 30],
+    ['MarcaF', 'verde', 22],
+    ['MarcaG', 'azul', 20]
+]
+
+print(pegaBicicletas(corMock, aroMock, listaMock))
+
